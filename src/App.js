40f20b213b5bd31bb941992aac9885272206dcb9
agpavlik <agpavlik@gmail.com>
2023-09-06 11:35:36 -0400

    Add readme

--- src/App.js
+++ src/App.js
@@ -9,36 +9,9 @@ function createRandomPost() {
   };
 }
 
-// // 1 step - Create a context provider
-// // Put component <PostContext.Provider> into return
-// const PostContext = createContext();
-
 function App() {
-  // const [posts, setPosts] = useState(() =>
-  //   Array.from({ length: 30 }, () => createRandomPost())
-  // );
-  // const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
-  // // Derived state. These are the posts that will actually be displayed
-  // const searchedPosts =
-  //   searchQuery.length > 0
-  //     ? posts.filter((post) =>
-  //         `${post.title} ${post.body}`
-  //           .toLowerCase()
-  //           .includes(searchQuery.toLowerCase())
-  //       )
-  //     : posts;
-
-  // function handleAddPost(post) {
-  //   setPosts((posts) => [post, ...posts]);
-  // }
-
-  // function handleClearPosts() {
-  //   setPosts([]);
-  // }
-
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
       document.documentElement.classList.toggle("fake-dark-mode");
